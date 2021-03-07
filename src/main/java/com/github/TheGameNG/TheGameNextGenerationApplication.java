662ed045acc092dea89d54e044c6fa6e1a3a1549
AleksandrB <bykov.alexnder@gmail.com>
2021-03-07 22:43:13 +0400

    initial commit

---
+++ src/main/java/com/github/TheGameNG/TheGameNextGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.github.TheGameNG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGameNextGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGameNextGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/TheGameNG/TheGameNextGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.TheGameNG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGameNextGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
